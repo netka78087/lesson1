7be7b2fd1d932a5c412241c7072459fff4253942
Natalia Sokolovskaya <netka78087@gmail.com>
2018-11-26 01:34:09 +0300

    Forth commit

--- vat.py
+++ vat.py
@@ -6,14 +6,11 @@
 # price_no_vat = price - vat
 # print(price_no_vat)
 
-# def get_vat(price, vat_rate):
-#     vat = price / 100 * vat_rate
-#     price_no_vat = price - vat
-#     print(price_no_vat)
+def get_vat(price, vat_rate):
+    vat = price / 100 * vat_rate
+    price_no_vat = price - vat
+    print(price_no_vat)
 
-# price1 = 100
-# vat_rate1 = 18
-# get_vat(price1, vat_rate1)
 price1 = 100
 vat_rate1 = 18
 get_vat(price1, vat_rate1)
@@ -22,5 +19,5 @@ price2 = 500
 vat_rate2 = 10
 get_vat(price2, vat_rate2)
 
-get_vat(50, '5')
+get_vat(50, 5)
 get_vat(-100, 18)
